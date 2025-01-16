d4dc4acc2f601c5155e9d823382d6cc447b4b747
PKdev <130464415+Pavlo1508@users.noreply.github.com>
2025-01-16 11:08:11 +0100

    edited hw4 ways

--- src/components/ImageCard/ImageCard.jsx
+++ src/components/ImageCard/ImageCard.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import s from './ImageCard.module.css'
-import ImageModal from '../../ImageModal/ImageModal';
+import ImageModal from '../ImageModal/ImageModal';
 
 const ImageCard = ({ url, altText, fullPhotoUrl }) => {
   const [isOpen, setIsOpen] = useState(false);
